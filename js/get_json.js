e143e69da5fac68e6b92136453b981e7fbe73c9d
Apridan Husaeni Muharam <97264739+apridanhm@users.noreply.github.com>
2022-07-14 08:10:42 +0700

    upddet jadi ke database

--- js/get_json.js
+++ js/get_json.js
@@ -1,11 +1,26 @@
+let apiUrl = "http://apiku.apridan.xyz/index.php/"
 Vue.createApp({
     data() {
       return {
         message: "Hello Vue!",
+        headers: {},
         artikel: [],
       };
     },
     methods: {
+      getHeadersData()
+      {
+        axios
+          .get(apiUrl+"users/1")
+          .then((res) => {
+            console.log(res.data); //melihat respon data pada console browser
+            this.artikel = res.data; //memperbarui variabel article pada bagian data()
+          })
+          .catch((error) => {
+            console.log(error); //melihat error jika pengambilan data adalah gagal
+          });
+      }, 
+
       getArticle()
       {
         axios
